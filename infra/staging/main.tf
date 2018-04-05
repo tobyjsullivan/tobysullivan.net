1ab15f8e48bfa73cf9d43adb4248bbdc2f0b065c
Toby Sullivan <tobyjsullivan@gmail.com>
2018-04-05 21:17:22 +1000

    Move state to S3 backend

---
+++ infra/staging/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-states.tobyjsullivan.com"
+    key    = "states/tobysullivan.net/terraform.staging.tfstate"
+    region = "us-east-1"
+  }
+}
+
+variable "cloudflare_email" {}
+variable "cloudflare_token" {}
+
+module "website" {
+  source = "../common/"
+  env = "staging"
+  domain = "staging.tobysullivan.net"
+  cloudflare_domain = "tobysullivan.net"
+  cloudflare_email = "${var.cloudflare_email}"
+  cloudflare_token = "${var.cloudflare_token}"
+}
